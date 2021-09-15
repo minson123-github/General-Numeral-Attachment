d47544435b6cfa717b45edb2f661f42ed245eb52
minson123-github <minson123.mail@gmail.com>
2021-09-15 15:53:55 +0800

    add category classifier

---
+++ support_task/category/RoBERTa-classifier/data_process.py
@@ -0,0 +1,167 @@
+import json
+import torch
+from tqdm import tqdm
+from torch.utils.data import DataLoader, Dataset
+
+class paragraphDataset(Dataset):
+	
+	def __init__(self, inputs, numeral_position, category_id):
+		self.inputs = torch.LongTensor(inputs)
+		self.numeral_position = numeral_position
+		self.category_id = torch.LongTensor(category_id)
+	
+	def __len__(self):
+		return len(self.inputs)
+	
+	def __getitem__(self, idx):
+		return self.inputs[idx], self.numeral_position[idx], self.category_id[idx]
+
+def collate_fn(batch):
+	batch_inputs, batch_numeral_position, batch_category_id = zip(*batch)
+	return torch.stack(batch_inputs), batch_numeral_position, torch.stack(batch_category_id)
+
+def get_all_category(args):
+	with open(args['data'], 'r') as fp:
+		data = json.load(fp)
+	category_set = set()
+	for d in data:
+		category_set.add(d['category'])
+	return [category for category in category_set]
+
+def get_train_dataloader(args, tokenizer):
+	category = get_all_category(args)
+	cate2id = {cate: i for i, cate in enumerate(category)}
+	with open(args['train_data'], 'r') as fp:
+		data = json.load(fp)
+	inputs, numeral_position, category_id = [], [], []
+
+	for d in tqdm(data, position = 0, desc='tokenize'):
+		paragraph_tokens = tokenizer(d['paragraph'], return_tensors='np', return_offsets_mapping=True, return_attention_mask=False, verbose=False)
+		L, R = -1, -1
+		for idx, offset in enumerate(paragraph_tokens.offset_mapping[0]):
+			if offset[0] <= d['offset_start'] and d['offset_start'] < offset[1]:
+				L = idx
+			if offset[0] < d['offset_end'] and d['offset_end'] <= offset[1]:
+				R = idx
+		offset_start, offset_end = -1, -1
+		for idx, offset in enumerate(paragraph_tokens.offset_mapping[0]):
+			if idx >= L - args['extend_token'] and (offset[0] != 0 or offset[1] != 0) and offset_start == -1:
+				offset_start = offset[0]
+			if idx <= R + args['extend_token'] and (offset[0] != 0 or offset[1] != 0):
+				offset_end = offset[1]
+
+		cate = cate2id[d['category']]
+		input_sentence = d['paragraph'][offset_start: d['offset_start']] + " " + tokenizer.mask_token + " " + d['paragraph'][d['offset_end']: offset_end]
+
+		input_tokens = tokenizer(input_sentence, return_tensors='np', padding='max_length', max_length=args['padding_length'], return_attention_mask=False)
+		numeral_p = 0
+		for idx, token_id in enumerate(input_tokens.input_ids[0]):
+			if token_id == tokenizer.mask_token_id:
+				numeral_p = idx
+				break
+
+		inputs.append(input_tokens.input_ids[0])
+		numeral_position.append(numeral_p)
+		category_id.append(cate)
+	
+	train_dataset = paragraphDataset(inputs, numeral_position, category_id)
+	train_dataloader = DataLoader(
+						train_dataset, 
+						args['batch_size'], 
+						shuffle=True, 
+						collate_fn=collate_fn, 
+						num_workers=4
+					)
+	return train_dataloader
+
+def get_eval_dataloader(args, tokenizer):
+	category = get_all_category(args)
+	cate2id = {cate: i for i, cate in enumerate(category)}
+	with open(args['eval_data'], 'r') as fp:
+		data = json.load(fp)
+	inputs, numeral_position, category_id = [], [], []
+
+	for d in tqdm(data, position = 0, desc='tokenize'):
+		paragraph_tokens = tokenizer(d['paragraph'], return_tensors='np', return_offsets_mapping=True, return_attention_mask=False, verbose=False)
+		L, R = -1, -1
+		for idx, offset in enumerate(paragraph_tokens.offset_mapping[0]):
+			if offset[0] <= d['offset_start'] and d['offset_start'] < offset[1]:
+				L = idx
+			if offset[0] < d['offset_end'] and d['offset_end'] <= offset[1]:
+				R = idx
+		offset_start, offset_end = -1, -1
+		for idx, offset in enumerate(paragraph_tokens.offset_mapping[0]):
+			if idx >= L - args['extend_token'] and (offset[0] != 0 or offset[1] != 0) and offset_start == -1:
+				offset_start = offset[0]
+			if idx <= R + args['extend_token'] and (offset[0] != 0 or offset[1] != 0):
+				offset_end = offset[1]
+
+		cate = cate2id[d['category']]
+		input_sentence = d['paragraph'][offset_start: d['offset_start']] + " " + tokenizer.mask_token + " " + d['paragraph'][d['offset_end']: offset_end]
+
+		input_tokens = tokenizer(input_sentence, return_tensors='np', padding='max_length', max_length=args['padding_length'], return_attention_mask=False)
+		numeral_p = 0
+		for idx, token_id in enumerate(input_tokens.input_ids[0]):
+			if token_id == tokenizer.mask_token_id:
+				numeral_p = idx
+				break
+
+		inputs.append(input_tokens.input_ids[0])
+		numeral_position.append(numeral_p)
+		category_id.append(cate)
+	
+	eval_dataset = paragraphDataset(inputs, numeral_position, category_id)
+	eval_dataloader = DataLoader(
+						eval_dataset, 
+						args['batch_size'], 
+						shuffle=False, 
+						collate_fn=collate_fn, 
+						num_workers=4
+					)
+	return eval_dataloader
+
+def get_test_dataloader(args, tokenizer):
+	category = get_all_category(args)
+	cate2id = {cate: i for i, cate in enumerate(category)}
+	with open(args['test_data'], 'r') as fp:
+		data = json.load(fp)
+	inputs, numeral_position, category_id = [], [], []
+
+	for d in tqdm(data, position = 0, desc='tokenize'):
+		paragraph_tokens = tokenizer(d['paragraph'], return_tensors='np', return_offsets_mapping=True, return_attention_mask=False, verbose=False)
+		L, R = -1, -1
+		for idx, offset in enumerate(paragraph_tokens.offset_mapping[0]):
+			if offset[0] <= d['offset_start'] and d['offset_start'] < offset[1]:
+				L = idx
+			if offset[0] < d['offset_end'] and d['offset_end'] <= offset[1]:
+				R = idx
+		offset_start, offset_end = -1, -1
+		for idx, offset in enumerate(paragraph_tokens.offset_mapping[0]):
+			if idx >= L - args['extend_token'] and (offset[0] != 0 or offset[1] != 0) and offset_start == -1:
+				offset_start = offset[0]
+			if idx <= R + args['extend_token'] and (offset[0] != 0 or offset[1] != 0):
+				offset_end = offset[1]
+
+		cate = cate2id[d['category']]
+		input_sentence = d['paragraph'][offset_start: d['offset_start']] + " " + tokenizer.mask_token + " " + d['paragraph'][d['offset_end']: offset_end]
+
+		input_tokens = tokenizer(input_sentence, return_tensors='np', padding='max_length', max_length=args['padding_length'], return_attention_mask=False)
+		numeral_p = 0
+		for idx, token_id in enumerate(input_tokens.input_ids[0]):
+			if token_id == tokenizer.mask_token_id:
+				numeral_p = idx
+				break
+
+		inputs.append(input_tokens.input_ids[0])
+		numeral_position.append(numeral_p)
+		category_id.append(cate)
+	
+	test_dataset = paragraphDataset(inputs, numeral_position, category_id)
+	test_dataloader = DataLoader(
+						test_dataset, 
+						args['batch_size'], 
+						shuffle=False, 
+						collate_fn=collate_fn, 
+						num_workers=4
+					)
+	return test_dataloader
---
+++ support_task/category/RoBERTa-classifier/run.py
@@ -0,0 +1,147 @@
+import os
+import torch
+import random
+import pickle
+import numpy as np
+from tqdm import tqdm
+from model import RobertaNumeralClassifier
+from transformers import RobertaTokenizerFast, AdamW
+from config import get_config
+from data_process import get_train_dataloader, get_eval_dataloader, get_test_dataloader, get_all_category
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+def seed_everything(seed):
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed(seed)
+	torch.cuda.manual_seed_all(seed)
+	np.random.seed(seed)
+	random.seed(seed)
+	torch.backends.cudnn.benchmark = False
+	torch.backends.cudnn.deterministic = True
+
+def train(args):
+	seed_everything(args['seed'])
+	tokenizer = RobertaTokenizerFast.from_pretrained(args['pretrained_model'])
+	category = get_all_category(args)
+
+	model = RobertaNumeralClassifier.from_pretrained(args['pretrained_model'], num_labels=len(category))
+	model.to('cuda')
+
+	train_dataloader = get_train_dataloader(args, tokenizer)
+
+	save_dir = os.path.join(args['model_dir'], 'model')
+	if not os.path.exists(save_dir):
+		os.makedirs(save_dir)
+	
+	step_cnt = 0
+
+	optimizer = AdamW(model.parameters(), lr=args['lr'])
+
+	loss_fn = torch.nn.CrossEntropyLoss()
+	
+	model.train()
+	for epoch in range(args['n_epoch']):
+		pbar = tqdm(train_dataloader, position=0, desc='Epoch {}'.format(epoch))
+		n_correct, total = 0, 0
+		for batch in pbar:
+			optimizer.zero_grad()
+
+			inputs, numeral_position, category_id = batch
+			logits = model(inputs.cuda(), numeral_position).cpu()
+			loss = loss_fn(logits, category_id)
+			for logit, label in zip(logits, category_id):
+				max_arg = 0
+				for idx in range(len(logit)):
+					if logit[idx].item() > logit[max_arg].item():
+						max_arg = idx
+				if max_arg == label.item():
+					n_correct += 1
+				total += 1
+			acc = n_correct / total
+			pbar.set_postfix({'acc': '{:.3f}%'.format(acc * 100), 'loss': '{:.3f}'.format(loss.item())})
+			step_cnt += 1
+
+			loss.backward()
+			optimizer.step()
+
+			if step_cnt % args['saving_steps'] == 0:
+				ckpt_dir = os.path.join(args['model_dir'], 'checkpoints')
+				if not os.path.exists(ckpt_dir):
+					os.makedirs(ckpt_dir)
+				ckpt_name = 'ckpt_{}'.format(step_cnt // args['saving_steps'])
+				ckpt_path = os.path.join(ckpt_dir, ckpt_name)
+				model.save_pretrained(ckpt_path)
+	
+	model.save_pretrained(save_dir)
+
+def test(args):
+	category = get_all_category(args)
+	seed_everything(args['seed'])
+
+	tokenizer = RobertaTokenizerFast.from_pretrained(args['pretrained_model'])
+	test_dataloader = get_test_dataloader(args, tokenizer)
+	model = RobertaNumeralClassifier.from_pretrained(args['testing_model'], num_labels=len(category))
+	'''
+	with open('model.pickle', 'rb') as fp:
+		model = pickle.load(fp)
+	'''
+	model.to('cuda')
+	model.eval()
+	loss_fn = torch.nn.CrossEntropyLoss()
+	
+	log = []
+	n_correct, total = 0, 0
+	with torch.no_grad():
+		pbar = tqdm(test_dataloader, position=0, desc='inference')
+		for batch in pbar:
+			inputs, numeral_position, category_id = batch
+			logits = model(inputs.cuda(), numeral_position).cpu()
+			loss = loss_fn(logits, category_id)
+			for logit, label in zip(logits, category_id):
+				max_arg = 0
+				for idx in range(len(logit)):
+					if logit[idx].item() > logit[max_arg].item():
+						max_arg = idx
+				if max_arg == label.item():
+					n_correct += 1
+				total += 1
+				log.append((category[max_arg], category[label.item()]))
+			acc = n_correct / total
+			pbar.set_postfix({'acc': '{:.3f}%'.format(acc * 100), 'loss': '{:.3f}'.format(loss.item())})
+	
+	with open('log/test-log.txt', 'w') as fp:
+		for p, r in log:
+			fp.write('{} | {}\n'.format(p, r))
+			
+	log = []
+	n_correct, total = 0, 0
+	eval_dataloader = get_eval_dataloader(args, tokenizer)
+	with torch.no_grad():
+		pbar = tqdm(eval_dataloader, position=0, desc='evaluate')
+		for batch in pbar:
+			inputs, numeral_position, category_id = batch
+			logits = model(inputs.cuda(), numeral_position).cpu()
+			loss = loss_fn(logits, category_id)
+			for logit, label in zip(logits, category_id):
+				max_arg = 0
+				for idx in range(len(logit)):
+					if logit[idx].item() > logit[max_arg].item():
+						max_arg = idx
+				if max_arg == label.item():
+					n_correct += 1
+				total += 1
+				log.append((category[max_arg], category[label.item()]))
+			acc = n_correct / total
+			pbar.set_postfix({'acc': '{:.3f}%'.format(acc * 100), 'loss': '{:.3f}'.format(loss.item())})
+	
+	with open('log/eval-log.txt', 'w') as fp:
+		for p, r in log:
+			fp.write('{} | {}\n'.format(p, r))
+
+if __name__ == '__main__':
+	args = get_config()
+	if args['mode'] == 'train':
+		train(args)
+	if args['mode'] == 'test':
+		test(args)
