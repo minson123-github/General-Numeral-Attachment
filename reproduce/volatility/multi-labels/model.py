9876dea82c3b189de6f4ddd49575d9ff87b778c5
minson123-github <minson123.mail@gmail.com>
2021-11-13 01:01:22 +0800

    add volatility multi-days predict model

---
+++ reproduce/volatility/multi-labels/model.py
@@ -0,0 +1,83 @@
+import math
+import torch
+
+class sentenceAttention(torch.nn.Module):
+    
+    def __init__(self, embed_dim: int, num_heads: int, dropout: float = 0.1):
+        super(sentenceAttention, self).__init__()
+        self.q_proj = torch.nn.Linear(embed_dim, embed_dim)
+        self.embed_proj = torch.nn.Linear(embed_dim, embed_dim)
+        self.atten = torch.nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout, batch_first=True)
+
+    def forward(self, sent_embed):
+        query = self.q_proj(sent_embed)
+        sent_embed = self.embed_proj(sent_embed)
+        query = torch.mean(query, 1, True)
+        attn_output, attn_output_weights = self.atten(query, sent_embed, sent_embed)
+        return torch.squeeze(attn_output, dim=1)
+
+class PositionalEncoding(torch.nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = torch.nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.pe = torch.nn.parameter.Parameter(pe)
+        #self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+class TransformerLayer(torch.nn.Module):
+
+    def __init__(self, init_dim, embed_dim: int, num_heads: int, dropout: float = 0.1):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.embed_proj = torch.nn.Sequential(
+            torch.nn.Linear(init_dim, embed_dim * 4), 
+            torch.nn.ReLU(), 
+            torch.nn.Linear(embed_dim * 4, embed_dim)
+        )
+        self.pos_encode = PositionalEncoding(embed_dim, dropout)
+        self.atten_layer = sentenceAttention(embed_dim, num_heads, dropout)
+        self.ffn = torch.nn.Sequential(
+            torch.nn.Linear(embed_dim, embed_dim * 4), 
+            torch.nn.ReLU(), 
+            torch.nn.Linear(embed_dim * 4, embed_dim)
+        )
+        self.dropout = torch.nn.Dropout(dropout)
+        self.layernorm1 = torch.nn.LayerNorm(embed_dim)
+        self.layernorm2 = torch.nn.LayerNorm(embed_dim)
+
+    def forward(self, sent_embed):
+        sent_embed = self.embed_proj(sent_embed)
+        sent_embed = sent_embed * math.sqrt(self.embed_dim)
+        sent_embed = self.pos_encode(sent_embed)
+        outputs = self.atten_layer(sent_embed)
+        out1 = self.layernorm1(outputs)
+        ffn_out = self.ffn(out1)
+        ffn_out = self.dropout(ffn_out)
+        return self.layernorm2(out1 + ffn_out)
+
+class LinearLayer(torch.nn.Module):
+    
+    def __init__(self, embed_dim: int, mid_dim: int):
+        super().__init__()
+        self.linear = torch.nn.Sequential(
+            torch.nn.Linear(embed_dim, mid_dim), 
+            torch.nn.ReLU(), 
+            torch.nn.Linear(mid_dim, 4)
+        )
+
+    def forward(self, inputs):
+        return self.linear(inputs)
---
+++ reproduce/volatility/multi-labels/run.py
@@ -0,0 +1,201 @@
+import os
+import time
+import torch
+import random
+import itertools
+import numpy as np
+from tqdm import tqdm
+from model import TransformerLayer, LinearLayer
+from transformers import RobertaModel, RobertaTokenizerFast, AdamW
+from config import get_config
+from data_process import get_train_dataloader, get_test_dataloader
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+def seed_everything(seed):
+    print('Setting global seed to {}'.format(seed))
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+def train(args):
+    seed_everything(args['seed'])
+    tokenizer = RobertaTokenizerFast.from_pretrained('roberta-large')
+
+    roberta_layer = RobertaModel.from_pretrained(args['pretrained_model'])
+    embed_dim = roberta_layer.config.hidden_size
+    transformer_layer = TransformerLayer(embed_dim, 1024, 16, 0)
+    linear_layer = LinearLayer(1024, 256)
+
+    transformer_layer.to('cuda')
+    linear_layer.to('cuda')
+
+    train_dataloader = get_train_dataloader(args, tokenizer, roberta_layer)
+    test_dataloader = get_test_dataloader(args, tokenizer, roberta_layer)
+
+    save_dir = os.path.join(args['model_dir'], 'model')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+    step_cnt = 0
+
+    optimizer = AdamW(
+        itertools.chain(
+            transformer_layer.parameters(), 
+            linear_layer.parameters()
+        ), 
+        lr=args['lr']
+    )
+
+    loss_fn = torch.nn.MSELoss()
+    
+    transformer_layer.train()
+    linear_layer.train()
+
+    best = 1000
+    best_epoch = -1
+    best_triple = (0, 0, 0)
+
+    for epoch in range(args['n_epoch']):
+        transformer_layer.train()
+        linear_layer.train()
+        pbar = tqdm(train_dataloader, position=0, desc='Epoch {}'.format(epoch))
+        total, size = 0, 0
+
+        for batch_embeds, batch_labels in pbar: 
+            outputs = transformer_layer(batch_embeds.cuda())
+            outputs = linear_layer(outputs).cpu()
+
+            loss = loss_fn(batch_labels, outputs)
+            total += loss.item()
+            size += 1
+            pbar.set_postfix({'mse': '{:.3f}'.format(total / size)})
+            step_cnt += 1
+
+            loss.backward()
+
+            if step_cnt % args['accumulation_steps'] == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+
+            if type(args['saving_steps']) != type(None) and step_cnt % args['saving_steps'] == 0:
+                ckpt_dir = os.path.join(args['model_dir'], 'checkpoints')
+                ckpt_name = 'step_ckpt_{}'.format(step_cnt // args['saving_steps'])
+                ckpt_path = os.path.join(ckpt_dir, ckpt_name)
+                if not os.path.exists(ckpt_path):
+                    os.makedirs(ckpt_path)
+                roberta_layer.save_pretrained(os.path.join(ckpt_path, 'roberta'))
+                torch.save(transformer_layer, os.path.join(ckpt_path, 'transformer.pt'))
+                torch.save(linear_layer, os.path.join(ckpt_path, 'linear.pt'))
+
+        transformer_layer.eval()
+        linear_layer.eval()
+        with torch.no_grad():
+            r3_total, r3_size = 0, 0
+            r7_total, r7_size = 0, 0
+            r15_total, r15_size = 0, 0
+            r30_total, r30_size = 0, 0
+            for batch_embeds, batch_labels in test_dataloader:
+                outputs = transformer_layer(batch_embeds.cuda())
+                outputs = linear_layer(outputs).cpu()
+                r3_outputs = outputs[:, 0]
+                r3_batch_labels = batch_labels[:, 0]
+                loss = loss_fn(r3_batch_labels, r3_outputs)
+                r3_total += loss.item()
+                r3_size += 1
+
+                r7_outputs = outputs[:, 1]
+                r7_batch_labels = batch_labels[:, 1]
+                loss = loss_fn(r7_batch_labels, r7_outputs)
+                r7_total += loss.item()
+                r7_size += 1
+
+                r15_outputs = outputs[:, 2]
+                r15_batch_labels = batch_labels[:, 2]
+                loss = loss_fn(r15_batch_labels, r15_outputs)
+                r15_total += loss.item()
+                r15_size += 1
+
+                r30_outputs = outputs[:, 3]
+                r30_batch_labels = batch_labels[:, 3]
+                loss = loss_fn(r30_batch_labels, r30_outputs)
+                r30_total += loss.item()
+                r30_size += 1
+            # print(r3_total / r3_size, r7_total / r7_size, r15_total / r15_size, r30_total / r30_size)
+            if (r3_total / r3_size + r7_total / r7_size + r15_total / r15_size + r30_total / r30_size) / 4 < best:
+                best = (r3_total / r3_size + r7_total / r7_size + r15_total / r15_size + r30_total / r30_size) / 4
+                best_triple = (r3_total / r3_size, r7_total / r7_size, r15_total / r15_size, r30_total / r30_size)
+                best_epoch = epoch
+                torch.save(transformer_layer, os.path.join(save_dir, 'transformer.pt'))
+                torch.save(linear_layer, os.path.join(save_dir, 'linear.pt'))
+
+        if type(args['saving_epochs']) != type(None) and (epoch + 1) % args['saving_epochs'] == 0:
+            ckpt_dir = os.path.join(args['model_dir'], 'checkpoints')
+            ckpt_name = 'epoch_ckpt_{}'.format((epoch + 1) // args['saving_epochs'])
+            ckpt_path = os.path.join(ckpt_dir, ckpt_name)
+            if not os.path.exists(ckpt_path):
+                os.makedirs(ckpt_path)
+            torch.save(transformer_layer, os.path.join(ckpt_path, 'transformer.pt'))
+            torch.save(linear_layer, os.path.join(ckpt_path, 'linear.pt'))
+    
+    print('Best test mse: {:.3f}, {:.3f}, {:.3f}, {:.3f}, Best epoch: {}'.format(best_triple[0], best_triple[1], best_triple[2], best_triple[3], best_epoch))
+
+def test(args):
+    seed_everything(args['seed'])
+
+    save_dir = os.path.join(args['model_dir'], 'model')
+    tokenizer = RobertaTokenizerFast.from_pretrained('roberta-large')
+    roberta_layer = RobertaModel.from_pretrained(args['pretrained_model'])
+    transformer_layer = torch.load(os.path.join(save_dir, 'transformer.pt'))
+    linear_layer = torch.load(os.path.join(save_dir, 'linear.pt'))
+    test_dataloader = get_test_dataloader(args, tokenizer, roberta_layer)
+
+    transformer_layer.to('cuda')
+    linear_layer.to('cuda') 
+    loss_fn = torch.nn.MSELoss()
+    transformer_layer.eval()
+    linear_layer.eval()
+    
+    with torch.no_grad():
+        r3_total, r3_size = 0, 0
+        r7_total, r7_size = 0, 0
+        r15_total, r15_size = 0, 0
+        r30_total, r30_size = 0, 0
+        for batch_embeds, batch_labels in test_dataloader:
+            outputs = transformer_layer(batch_embeds.cuda())
+            outputs = linear_layer(outputs).cpu()
+            r3_outputs = outputs[:, 0]
+            r3_batch_labels = batch_labels[:, 0]
+            loss = loss_fn(r3_batch_labels, r3_outputs)
+            r3_total += loss.item()
+            r3_size += 1
+
+            r7_outputs = outputs[:, 1]
+            r7_batch_labels = batch_labels[:, 1]
+            loss = loss_fn(r7_batch_labels, r7_outputs)
+            r7_total += loss.item()
+            r7_size += 1
+
+            r15_outputs = outputs[:, 2]
+            r15_batch_labels = batch_labels[:, 2]
+            loss = loss_fn(r15_batch_labels, r15_outputs)
+            r15_total += loss.item()
+            r15_size += 1
+
+            r30_outputs = outputs[:, 3]
+            r30_batch_labels = batch_labels[:, 3]
+            loss = loss_fn(r30_batch_labels, r30_outputs)
+            r30_total += loss.item()
+            r30_size += 1
+        print(r3_total / r3_size, r7_total / r7_size, r15_total / r15_size, r30_total / r30_size)
+
+if __name__ == '__main__':
+    args = get_config()
+    if args['mode'] == 'train':
+        train(args)
+    if args['mode'] == 'test':
+        test(args)
